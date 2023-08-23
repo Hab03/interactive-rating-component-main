1d5d791378b5cf708873834166c331b7a14cd7db
Hab03 <adityaganguly97@gmail.com>
2023-08-23 11:48:24 +0530

    Testing the rating key- grey when cked issue

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 let myIndex = -1;
 const handleClick = (e) => {
   myIndex = e;
+  if(e===1){
+    document.getElementsByClassName("one")[0].style.backgroundColor="hsl(217, 12%, 63%)";
+    document.getElementsByClassName("one")[0].style.color="white";
+  }
+  else if(e===2){
+    document.getElementsByClassName("two")[0].style.backgroundColor="hsl(217, 12%, 63%)";
+    document.getElementsByClassName("two")[0].style.color="white";
+  }
+  else if(e===3){
+    document.getElementsByClassName("three")[0].style.backgroundColor="hsl(217, 12%, 63%)";
+    document.getElementsByClassName("three")[0].style.color="white";
+  }
+  else if(e===4){
+    document.getElementsByClassName("four")[0].style.backgroundColor="hsl(217, 12%, 63%)";
+    document.getElementsByClassName("four")[0].style.color="white";
+  }
+  else if(e===5){
+    document.getElementsByClassName("five")[0].style.backgroundColor="hsl(217, 12%, 63%)";
+    document.getElementsByClassName("five")[0].style.color="white";
+  }
+
+
 };
 
 const handleSubmit = (e) => {
